1617b9452366d1e442fde552b7a7c3ea954add2b
drproject-coding <yfatihi.pro@gmail.com>
2026-03-06 00:54:37 +0100

    feat: export Heading, Text, Icon from components index

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,3 +1,12 @@
+export { Heading } from "./Heading";
+export type { HeadingProps } from "./Heading";
+
+export { Text } from "./Text";
+export type { TextProps } from "./Text";
+
+export { Icon } from "./Icon";
+export type { IconProps } from "./Icon";
+
 export { Button } from "./Button";
 export type { ButtonProps } from "./Button";
 
